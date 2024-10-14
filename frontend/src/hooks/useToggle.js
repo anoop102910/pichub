8e302e8adf49e71d89416d367699092cda2cb935
Anoop Singh <anoop102910@gmail.com>
2024-10-14 22:40:53 +0530

    update

--- frontend/src/hooks/useToggle.js
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-export default function useToggle(initial = false) {
-    const [isToggled, setIsToggled] = useState(initial);
-    const toggle = () => {
-        setIsToggled(prev => !prev);
-    };
-    return [isToggled, toggle];
-}
--- frontend/src/services/utils.js
+++
@@ -1,8 +0,0 @@
-import { fetchBaseQuery } from "@reduxjs/toolkit/query";
-export const baseQuery = fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api",
-    credentials: "include",
-    headers: {
-        Authorization: `${localStorage.getItem("token") ? `Bearer ${localStorage.getItem("token")}` : ""}`,
-    },
-});
